61ffde503f29183739f117dad80b92757f1dcd2a
Saurabh Mishra <Saurabh_mishra@epam.com>
2020-04-19 01:37:01 +0530

    update server client

--- src/main/java/com/epam/wfh/manager/client/ServerClient.java
+++ src/main/java/com/epam/wfh/manager/client/ServerClient.java
@@ -6,13 +6,15 @@ public class ServerClient {
 
 public static void postServerUpdate(String message){
 
-
+try {
     final String uri = "http://localhost:8080/workinghour";
 
     RestTemplate restTemplate = new RestTemplate();
 
-    restTemplate.postForObject( uri, message, String.class);
-
+    restTemplate.postForObject(uri, message, String.class);
+}catch (Exception ex){
+    System.out.println("failure in sending server update"+ex);
+}
 }
 
 }

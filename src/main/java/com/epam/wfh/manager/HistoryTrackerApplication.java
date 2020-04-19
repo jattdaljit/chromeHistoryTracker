9490723cc41f85b8ed817ce1235528b143e3e6b9
Daljit Singh <daljitsingh1101@gmail.com>
2020-04-19 13:54:11 +0530

    Added extra processing logic to chrome history. Added Class to process windows-exp.exe data

--- src/main/java/com/epam/wfh/manager/HistoryTrackerApplication.java
+++ src/main/java/com/epam/wfh/manager/HistoryTrackerApplication.java
@@ -21,24 +21,28 @@ public class HistoryTrackerApplication {
 
 		DataProcessor dataProcessor = new DataProcessor();
 		List<HistoryData> list = dataProcessor.processSqlData(resultSet);
-		dataProcessor.printData(list);
-		Integer totalTime = dataProcessor.calculateTotalTime(list);
-		Integer productiveTime = dataProcessor.getProductiveTime(list);
-		System.out.println("Productive minutes:  " + productiveTime/60);
-		System.out.println("Non-Productive minutes: "+ (totalTime-productiveTime)/60);
+		//dataProcessor.printData(list);
+		Map<String,Integer> map = dataProcessor.getWebsiteVisitFrequency(list);
+//		for(Map.Entry<String,Integer> entry : map.entrySet()){
+//			System.out.println("URL: " + entry.getKey() + " Count: " + entry.getValue() );
+//		}
+//		Integer totalTime = dataProcessor.calculateTotalTime(list);
+//		Integer productiveTime = dataProcessor.getProductiveTime(list);
+//		System.out.println("Productive minutes:  " + productiveTime/60);
+//		System.out.println("Non-Productive minutes: "+ (totalTime-productiveTime)/60);
 
 
-		//sending notification
-		WindowsNotification windowsNotification = new WindowsNotification();
-		windowsNotification.showMessage("Productive minutes: " + productiveTime/60 + "\n"
-		+ "Non-Productive minutes: "+ (totalTime-productiveTime)/60);
-
+//		//sending notification
+//		WindowsNotification windowsNotification = new WindowsNotification();
+//		windowsNotification.showMessage("Productive minutes: " + productiveTime/60 + "\n"
+//		+ "Non-Productive minutes: "+ (totalTime-productiveTime)/60);
+//
 		//creating chart image out of data
-		Map<String, Integer> map = new HashMap<>();
-		map.put("Productive Minutes", productiveTime/60);
-		map.put("Non Productive Minutes", (totalTime-productiveTime)/60);
+		//Map<String, Integer> map = new HashMap<>();
+//		map.put("Productive", productiveTime/60);
+//		map.put("Non Productive", (totalTime-productiveTime)/60);
 		PieChart pieChart = new PieChart();
-		pieChart.createChartImage(map);
+		pieChart.createChartImage(map, "");
 
 
 	}
--- src/main/java/com/epam/wfh/manager/service/PieChart.java
+++ src/main/java/com/epam/wfh/manager/service/PieChart.java
@@ -6,16 +6,18 @@ import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Map;
 
 public class PieChart {
 
-    private PieDataset createData(Map<String, Integer> map) {
+    private PieDataset createData(Map<String, Integer> map, String timeUnit) {
         DefaultPieDataset data = new DefaultPieDataset();
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            data.setValue(entry.getKey() + ":\n " + Double.valueOf(map.get(entry.getKey())), Double.valueOf(map.get(entry.getKey())));
+            data.setValue(entry.getKey().toUpperCase() + ":\n " + Double.valueOf(map.get(entry.getKey()))
+                    + " " + timeUnit.toLowerCase(), Double.valueOf(map.get(entry.getKey())));
         }
         return data;
     }
@@ -26,13 +28,13 @@ public class PieChart {
         return chart;
     }
 
-    public void createDemoPanel(Map<String, Integer> map){
-        JFreeChart chart = createChart(createData(map));
+    public void createDemoPanel(Map<String, Integer> map, String timeUnit){
+        JFreeChart chart = createChart(createData(map, timeUnit));
         createImageOutOfChart(chart);
     }
 
-    public void createChartImage(Map<String, Integer> map){
-        createDemoPanel(map);
+    public void createChartImage(Map<String, Integer> map, String timeUnit){
+        createDemoPanel(map, timeUnit);
     }
 
     public void createImageOutOfChart(JFreeChart chart) {
